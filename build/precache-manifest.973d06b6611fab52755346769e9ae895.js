2fbd58e897d80ece40b523c6dc6848b0d5a0d789
yaya <anbesie.yg@gmail.com>
2020-07-03 03:34:23 -0700

    updated

---
+++ build/precache-manifest.973d06b6611fab52755346769e9ae895.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a165727d54d09d70d2d373c3d54d0839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75d7b96b200a9c70a60a",
+    "url": "/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "1dcabcf8ddd5746f027b",
+    "url": "/static/css/main.1136b3e4.chunk.css"
+  },
+  {
+    "revision": "75d7b96b200a9c70a60a",
+    "url": "/static/js/2.04c816c2.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/static/js/2.04c816c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dcabcf8ddd5746f027b",
+    "url": "/static/js/main.6a135b74.chunk.js"
+  },
+  {
+    "revision": "16ae9854644794f32750",
+    "url": "/static/js/runtime-main.89434f27.js"
+  },
+  {
+    "revision": "6f165089723c4851ed4c0b9548717654",
+    "url": "/static/media/patientAvatar.6f165089.jpeg"
+  }
+]);
