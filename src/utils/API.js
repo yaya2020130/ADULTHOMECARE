df9b87115c0f7d251b01b205be506efe8230b677
yaya <anbesie.yg@gmail.com>
2020-07-05 23:57:27 -0700

    updated

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import logInbtn from "../components/LogInBtn"; 
-// const BASE_URL = "http://localhost:8080"
-const BASE_URL="https://adulthomecare-backend.herokuapp.com/"
+const BASE_URL = "http://localhost:8080"
+// const BASE_URL="https://adulthomecare-backend.herokuapp.com/"
 export default {
   getPatientData: (id) => axios.get(`${BASE_URL}/patient/${id}`),
   // getPatientLog: (id) => axios.get(`${BASE_URL}/log/${id}`),
